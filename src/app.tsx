58833358894ef15837092e62eace256716d031e5
RiverDot <72624705+RiverDot@users.noreply.github.com>
2023-10-11 13:04:54 -0400

    Made The Website A PWA

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { useState } from 'preact/hooks'
 import { BackGround } from './components/backGround'
 import { TitleCard } from './components/titleCard'
 import './app.css'
+import { ReloadPrompt } from './ReloadPrompt'
 
 export function App() {
   const [started, setStarted] = useState(false)
@@ -14,18 +15,19 @@ export function App() {
     console.log(name)
     getPronouns(pronouns)
     console.log(started)
-    setStarted(true)
   }
 
   async function getPronouns(pronouns: string) {
     const response = await fetch(`https://en.pronouns.page/api/pronouns/${pronouns}`);
     const pronounData = await response.json();
     console.log(pronounData);
+    setStarted(true)
   }
 
 
   return (
     <>
+      <ReloadPrompt />
       <BackGround />
       <div class="h-screen grid grid-cols-3 gap-4 content-center">
         <TitleCard title='Welcome to the Name Game' startFunc={StartGame} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,61 @@
 import { defineConfig } from 'vite'
+import packageJson from './package.json';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import preact from '@preact/preset-vite'
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'The Name Game',
+    short_name: 'Name Game',
+    theme_color: '#2196f3',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA(pwaOptions)
+  ],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ...renderChunks(packageJson.dependencies),
+        },
+      },
+    },
+  },
 })
